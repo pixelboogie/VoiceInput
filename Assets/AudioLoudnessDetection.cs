3452d0835ef40bd0856d48e6894aa72bdadbd0d7
pixelboogie <justamovie@yahoo.com>
2022-12-19 16:16:42 -0600

    Uses Mic

--- Assets/AudioLoudnessDetection.cs
+++ Assets/AudioLoudnessDetection.cs
@@ -6,10 +6,11 @@ public class AudioLoudnessDetection : MonoBehaviour
 {
 
     public int sampleWindow = 64;
+    private AudioClip microphoneClip;
 
     void Start()
     {
-        
+        MicrophoneToAudioClip();
     }
 
 
@@ -18,12 +19,25 @@ public class AudioLoudnessDetection : MonoBehaviour
         
     }
 
+    public void MicrophoneToAudioClip(){
+
+        // Get the first mircophone in device list
+        string microphoneName = Microphone.devices[0];
+        microphoneClip = Microphone.Start(microphoneName, true, 20, AudioSettings.outputSampleRate);
+    }
+
+    public float GetLoudnessFromMicrophone(){
+        return GetLoudnessFromAudioClip(Microphone.GetPosition(Microphone.devices[0]), microphoneClip);
+    }
+
+
+
     public float GetLoudnessFromAudioClip(int clipPosition, AudioClip clip){
         int startPosition = clipPosition - sampleWindow;
 
         if(startPosition < 0)
             return 0;
-            
+
         float[] waveData = new float[sampleWindow];
         clip.GetData(waveData,startPosition);
 
